c33380923e98d8a7616f7d893f3b70f3685816de
rama <rama@javan.co.id>
2022-12-05 22:14:23 +0700

    feat: development support

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
+            valetTls: 'lab-laravel.test',
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
