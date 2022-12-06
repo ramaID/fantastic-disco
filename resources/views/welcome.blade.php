151fe2e0d7ec1ab4befc383ae171b955a547b6bf
rama <rama@javan.co.id>
2022-12-06 18:01:54 +0700

    fix: wrong state

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,16 +38,16 @@
                             aria-controls="mobile-menu">
                             <span class="sr-only">Open main menu</span>
                             <!-- Menu open: "hidden", Menu closed: "block" -->
-                            <svg x-show="open" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                bind:aria-hidden="!open.toString()">
+                            <svg x-show="!open" bind:aria-hidden="!open.toString()" class="h-6 w-6"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
                             </svg>
                             <!-- Menu open: "block", Menu closed: "hidden" -->
-                            <svg x-show="!open" class="h-6 w-6 block" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                bind:aria-hidden="open.toString()">
+                            <svg x-show="open" bind:aria-hidden="open.toString()" class="h-6 w-6 block"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
                             </svg>
                         </button>
